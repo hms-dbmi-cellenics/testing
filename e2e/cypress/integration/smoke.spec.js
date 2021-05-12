baadbb17518a79a59a2e8a23ba790802f45b5287
Xavier Verges <460418+xverges@users.noreply.github.com>
2021-05-12 10:10:06 +0200

    Smoke test, wip

---
+++ e2e/cypress/integration/smoke.spec.js
@@ -0,0 +1,90 @@
+context('Smoke Test', () => {
+
+    const EXPERIMENT_ID = Cypress.env('EXPERIMENT_ID') ?? 'e52b39624588791a7889e39c617f669e';
+    const GENE_IN_TOOLS = Cypress.env('GENE_IN_TOOLS') ?? 'Lyz2';
+ 
+    it.only('runs the pipeline and can move to data exploration', () => {
+
+      cy.visit(`/experiments/${EXPERIMENT_ID}/data-processing`);
+      
+      // Verify that the first filter is disabled by prefiltered samples, and move past it
+      // cy.get('[role=alert]')
+      //   .should('contain.text', 'pre-filtered');
+      cy.get('[data-testid=enableFilterButton]')
+        .should('not.be.enabled');
+      cy.get('[data-testid=pipelineNextStep]').click();
+
+      // Enable+disable+save changes to trigger the pipeline
+      cy.get('[data-testid=enableFilterButton]').click();
+      cy.get('[data-testid=enableFilterButton]').click();
+      cy.get('[data-testid=runFilterButton]').click();
+
+      const numSteps = 7;
+      const stepTimeOut = 60 * 1000;
+      const oneStepTimeout = { timeout: stepTimeOut };
+      const restOfStepsTimeout = { timeout: stepTimeOut * (numSteps - 1) };
+      
+      // Wait for the pipeline to start.
+      // Starting the pipeline can result in us getting in a skeleton state,
+      // so we need a higher timeout also to get the progressbar
+      cy.get('[role=progressbar]', oneStepTimeout) 
+        .invoke(oneStepTimeout, 'attr', 'aria-valuenow')
+        .should('equal', '1');
+      // Wait for the pipeline to complete
+      cy.get('[role=progressbar]', )
+        .invoke(restOfStepsTimeout, 'attr', 'aria-valuenow')
+        .should('equal', `${numSteps}`);
+
+      // Move to Data Exploration 
+      cy.visit(`/experiments/${EXPERIMENT_ID}/data-exploration`);
+      const dataExplorationTimeOut = { timeout: 30 * 1000 };
+
+      // The clusters are loaded
+      // Mosaic is not very testing friendly :-(
+      cy.get('.mosaic-window', dataExplorationTimeOut)
+        .should('contain.text', 'Data Management')
+        .and('contain.text', 'Cluster 1')
+
+      // The list of gens is loaded so that we know that the worker has done something
+      const maxRetryAttempts = 3;
+      cy.get('.mosaic-window', dataExplorationTimeOut)
+        .contains('Tools')
+        .then(($toolsTitle) => {
+          const $tools = $toolsTitle.parent().parent();
+          const verifyGeneDisplayed = () => {
+            cy.wrap($tools).should('contain.text', GENE_IN_TOOLS);
+          };
+          const waitForWaitingBannerToGoAway = (callBack) => {
+            cy.wrap($tools, dataExplorationTimeOut)
+              .should('not.contain', 'getting your data')
+              .then(callBack);
+
+          };
+          const clickOnRetyButton = () => {
+            cy.wrap($tools)
+              .contains('button', /try again/i)
+              .click();
+          };
+          let attempts = 0;
+          function checkAndRetry() {
+            // synchronous queries, using jQuery
+            const tryAgainButton = $tools.find(':contains("Try again")');
+            const waitingBanner = $tools.find(':contains("getting your data")');
+            attempts += 1;
+            if (attempts > maxRetryAttempts) {
+              throw new Error('Number of retry attemps exceeded. Is there something wrong with the worker?');
+            }
+            else if (tryAgainButton.length) {
+              clickOnRetyButton();
+              cy.wait(100).then(checkAndRetry);
+            } else if (waitingBanner.length) {
+              waitForWaitingBannerToGoAway(checkAndRetry);
+            } else {
+              verifyGeneDisplayed();
+            }
+          }
+          checkAndRetry();
+        });
+    })
+  })
+  
