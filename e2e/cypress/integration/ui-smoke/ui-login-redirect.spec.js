804e8468455afb0b4a500a81257649ac2ea3b24f
StefanBabukov <stefanbabukov98@gmail.com>
2021-07-14 11:45:53 +0300

    [BIOMAGE-1050] Add authentication to the integration test (#2)
    * should be done
    
    * small changes
    
    * Delete package-lock.json
    
    remove package lock

--- e2e/cypress/integration/ui-smoke/ui-login-redirect.spec.js
+++ e2e/cypress/integration/ui-smoke/ui-login-redirect.spec.js
@@ -1,11 +1,27 @@
 /// <reference types="cypress" />
+import '../../support/commands';
 
 describe('Redirects to login page when unauthenticated', () => {
+  before(() => {
+    cy.clearLocalStorageSnapshot();
+  });
+
   beforeEach(() => {
-    cy.visit('/data-management');
+    cy.restoreLocalStorage();
+  });
+
+  afterEach(() => {
+    cy.saveLocalStorage();
   });
 
   it('redirects to a login page', () => {
+    cy.visit('/data-management');
     cy.url().should('include', 'auth.eu-west-1.amazoncognito.com');
+  });
+
+  it('Logging in', () => {
+    cy.visit('/not_valid_url', { failOnStatusCode: false });
+    cy.login();
+    cy.visit('/data-management');
   });
 });
