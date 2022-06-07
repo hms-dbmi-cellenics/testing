6046a71137bc867880252a968562accbbe0d1a3e
Pol Alvarez <pol.avms@gmail.com>
2022-06-07 11:06:06 +0200

    launch script

--- e2e/upload_dataset.py
+++ e2e/upload_dataset.py
@@ -17,12 +17,23 @@ def upload_data(elem):
     env["CYPRESS_E2E_PASSWORD"] = password
     subprocess.check_call("npm start", shell=True, env=env, stdout=sys.stdout, stderr=subprocess.STDOUT)
 
-df = pd.read_csv('./05_03_2022_users_list.csv.out')
+def upload_to_user(i):
+    # email="lbriciomoreno@mgh.harvard.edu"
+    # password="Tmp_2022-05-03OG3B7CJI"
+    env = os.environ.copy()
+    email="alex@biomage.net"
+    password="Tmp_2021_fAIFAOx"
 
-for i, elem in list(df.iterrows()):
-    print(f"{i}: {elem.email}")
-    upload_data(elem)
-#    p = multiprocessing.Pool(4)
+    env["CYPRESS_E2E_USERNAME"] = email
+    env["CYPRESS_E2E_PASSWORD"] = password
+    env["CYPRESS_E2E_ALIAS"] = f"{i}"
+    subprocess.check_call("npm start", shell=True, env=env, stdout=sys.stdout, stderr=subprocess.STDOUT)
 
-#     # map list to target function
-#     result = p.map(square, mylist)
+# df = pd.read_csv('./05_03_2022_users_list.csv.out')
+
+# for i, elem in list(df.iterrows()):
+#     print(f"{i}: {elem.email}")
+#     upload_data(elem)
+
+for i in range(4, 32):
+    upload_to_user(i)
