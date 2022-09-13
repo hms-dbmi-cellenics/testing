18e457dd7cc4f27c8ab900fc8d87298caa5a0996
stefanbabukov <stefanbabukov98@gmail.com>
2022-09-13 14:03:10 +0100

    test

--- e2e/cypress/plugins/index.js
+++ e2e/cypress/plugins/index.js
@@ -32,7 +32,7 @@ module.exports = async (on, config) => {
   const sandboxId = process.env.SANDBOX_ID || 'default';
 
   const UserPools = await userPoolClient.send(new ListUserPoolsCommand({ MaxResults: 60 }));
-  console.log('user pool', UserPools);
+  console.log('user pool', UserPools, 'key ', process.env.AWS_ACCESS_KEY_ID);
   // setting the user pool id
   const environment = process.env.K8S_ENV || 'development';
   const cognitoEnv = environment === 'development' ? 'staging' : environment; // development env uses staging cognito pool
