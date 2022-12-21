e09827db55cb782b793d1449f0417cad1ba4ae30
stefanbabukov <stefanbabukov98@gmail.com>
2022-12-21 13:37:43 +0200

    more tests added

--- e2e/cypress/integration/ui-smoke/api-endpoints.spec.js
+++ e2e/cypress/integration/ui-smoke/api-endpoints.spec.js
@@ -1,23 +1,28 @@
 import '../../support/commands';
+import { v4 as uuidv4 } from 'uuid';
 
 describe('API endpoints work as expected', () => {
-  beforeEach(() => {
+  before(() => {
     cy.login();
   });
 
-  it('Api urls work', () => {
-    const jwt = localStorage.getItem('idToken');
-    const { apiUrl } = Cypress.config();
-    const experimentId = '38eacf24-3f53-45ab-997d-36df0944f05d';
+  const { apiUrl } = Cypress.config();
+  const experimentId = '38eacf24-3f53-45ab-997d-36df0944f05d';
+  const jwt = localStorage.getItem('idToken');
 
-    // create experiment endpoint
+  const getRequestHeader = () => ({
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${jwt}`,
+  });
+
+  it('Api endpoints work for experiments', () => {
+    // create experiment
+    const requestHeaders = getRequestHeader();
+
     cy.request({
       method: 'POST',
       url: `${apiUrl}/v2/experiments/${experimentId}`,
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${jwt}`,
-      },
+      headers: requestHeaders,
       body: JSON.stringify({
         id: experimentId,
         name: 'randomName',
@@ -27,26 +32,108 @@ describe('API endpoints work as expected', () => {
       expect(response.status).equal(200);
     });
 
-    // get experiment endpoint
+    // get experiment
     cy.request({
       method: 'GET',
       url: `${apiUrl}/v2/experiments/${experimentId}`,
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${jwt}`,
-      },
+      headers: requestHeaders,
     }).then((response) => {
       expect(response.status).equal(200);
     });
 
-    // delete experiment
+    // patch experiment
+    cy.request({
+      method: 'PATCH',
+      url: `${apiUrl}/v2/experiments/${experimentId}`,
+      headers: requestHeaders,
+      body: JSON.stringify({
+        name: 'newPatchedExperimentName',
+      }),
+    }).then((response) => {
+      expect(response.status).equal(200);
+    });
+
+    // create sample file
+    cy.request({
+      method: 'POST',
+      url: `${apiUrl}/v2/experiments/${experimentId}/samples`,
+      headers: requestHeaders,
+      body: JSON.stringify([{
+        name: 'sampleName',
+        sampleTechnology: '10x',
+        options: {},
+      }]),
+    }).then((response) => {
+      const sampleId = response.body.sampleName;
+      expect(Object.keys(response.body)[0]).equal('sampleName');
+
+      // create sample file
+      cy.request({
+        method: 'POST',
+        url: `${apiUrl}/v2/experiments/${experimentId}/samples/${sampleId}/sampleFiles/barcodes10x`,
+        headers: requestHeaders,
+        body: JSON.stringify({
+          sampleFileId: uuidv4(),
+          size: 420,
+          metadata: {},
+        }),
+      }).then((createSampleFileResponse) => {
+        expect(createSampleFileResponse.status).equal(200);
+      });
+
+      // patch sample
+      cy.request({
+        method: 'PATCH',
+        url: `${apiUrl}/v2/experiments/${experimentId}/samples/${sampleId}`,
+        headers: requestHeaders,
+        body: JSON.stringify({
+          name: 'newPatchedName',
+        }),
+      }).then((patchSampleResponse) => {
+        expect(patchSampleResponse.status).equal(200);
+      });
+
+      // patch sample file
+      cy.request({
+        method: 'PATCH',
+        url: `${apiUrl}/v2/experiments/${experimentId}/samples/${sampleId}/sampleFiles/barcodes10x`,
+        headers: requestHeaders,
+        body: JSON.stringify({
+          uploadStatus: 'uploaded',
+        }),
+      }).then((patchSampleFileResponse) => {
+        expect(patchSampleFileResponse.status).equal(200);
+      });
+
+      // delete sample
+      cy.request({
+        method: 'DELETE',
+        url: `${apiUrl}/v2/experiments/${experimentId}/samples/${sampleId}`,
+        headers: requestHeaders,
+      }).then((deleteSampleResponse) => {
+        expect(deleteSampleResponse.status).equal(200);
+      });
+    });
+
+    // get userpool client id
+    cy.request({
+      method: 'GET',
+      url: `${apiUrl}/v2/programmaticInterfaceClient`,
+      headers: requestHeaders,
+    }).then((response) => {
+      expect(response.status).equal(200);
+      const returnedKeys = Object.keys(response.body);
+      expect(returnedKeys[0]).equal('clientId');
+      expect(returnedKeys[1]).equal('clientRegion');
+    });
+  });
+
+  after(() => {
+    // deleting created experiment
     cy.request({
       method: 'DELETE',
       url: `${apiUrl}/v2/experiments/${experimentId}`,
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${jwt}`,
-      },
+      headers: getRequestHeader(),
     }).then((response) => {
       expect(response.status).equal(200);
     });
